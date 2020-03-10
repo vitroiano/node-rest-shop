ebe5c6c5eb0990cdd437571caf2c93b4e9409eda
Vinicius Trainotti <vtrainotti@live.com>
2020-03-10 16:20:09 -0300

    Adicionado conexao docker local moongodb

--- app.js
+++ app.js
@@ -7,14 +7,26 @@ const mongoose = require('mongoose');
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
-// problem conexao
+// conexao MongoDB Atlas
+// mongoose.connect(
+//     "mongodb+srv://node-shop:" + process.env.MONGO_ATLAS_PW + "@cluster0-vfvxp.mongodb.net/test?retryWrites=true&w=majority",
+//     { 
+//         //useMongoClient: true,
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true 
+//     }, function(error){console.log(error)}
+// );
+
+// conexao MongoDB local
 mongoose.connect(
-    "mongodb+srv://node-shop:" + process.env.MONGO_ATLAS_PW + "@cluster0-vfvxp.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb://mongoadmin:" + process.env.MONGO_ATLAS_PW + "@172.17.0.4:27017/ecommerce?authSource=admin",
     { 
         //useMongoClient: true,
         useNewUrlParser: true,
-        useUnifiedTopology: true 
-    }, function(error){console.log(error)}
+        useUnifiedTopology: true
+    }, function(error){
+        //console.log(error)
+    }
 );
 
 mongoose.connection.on('connected', () => {
