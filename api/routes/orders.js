146448e4fa162fd7a9f08cf686d75e62b2778524
Vinicius Trainotti <vtrainotti@live.com>
2020-03-11 14:29:50 -0300

    JWT Route Protection

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const checkAuth = require('../middleware/check-auth')
 
 const Order = require('../models/order');
 const Product = require('../models/product');
@@ -33,7 +34,7 @@ router.get('/', (req, res, next) => {
         });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
 
     Product.findById(req.body.productId)
         .then(product => {
@@ -97,7 +98,7 @@ router.get('/:orderId', (req, res, next) => {
         });
 });
 
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
     Order.remove({ _id: req.params.orderId })
     .exec()
     .then(result => {
